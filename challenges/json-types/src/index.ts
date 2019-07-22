52a79a5c1a3dd6810e3ec81d9a161f01831e6abb
kylebirns <kylebirns@gmail.com>
2019-07-22 17:09:55 -0400

    feat(workshop): followed along

--- challenges/json-types/src/index.ts
+++ challenges/json-types/src/index.ts
@@ -1,1 +1,8 @@
 // 💡 HINT: number[] and Array<number> mean the same thing
+
+type JSONPrimitive = number | string | boolean | null;
+export type JSONValue = JSONPrimitive | JSONArray | JSONObject;
+export interface JSONArray extends Array<JSONValue> {}
+export interface JSONObject {
+    [k: string]: undefined | JSONValue
+}
